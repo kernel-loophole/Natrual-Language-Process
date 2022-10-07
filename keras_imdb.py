cfd15ae9754d546eb2cca6451facca1cb840b2d5
main <az4860364@gmail.com>
2022-10-07 06:22:09 -0400

    imdb data set code

--- keras_imdb.py
+++ keras_imdb.py
@@ -17,5 +17,28 @@ for label_type in ['neg', 'pos']:
             labels.append(0)
         else:
             labels.append(1)
+maxlen = 100
+training_samples = 200
+validation_samples = 10000
+max_words = 10000
+tokenizer = Tokenizer(num_words=max_words)
+tokenizer.fit_on_texts(texts)
+sequences = tokenizer.texts_to_sequences(texts)
+word_index = tokenizer.word_index
+print('Found %s unique tokens.' % len(word_index))
+data = pad_sequences(sequences, maxlen=maxlen)
+labels = np.asarray(labels)
+print('Shape of data tensor:', data.shape)
+print('Shape of label tensor:', labels.shape)
+indices = np.arange(data.shape[0])
+np.random.shuffle(indices)
+data = data[indices]
+labels = labels[indices]
+x_train= data[:training_samples]
+y_train= labels[:training_samples]
+x_val=data[training_samples: training_samples + validation_samples]
+y_val=labels[training_samples: training_samples + validation_samples]
+print(x_train)
+print(y_train)
 # print(labels)
 # print(dir_name)
